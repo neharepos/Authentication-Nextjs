1ba5fbdbf0f1548a9fadad4fe18841f098a28412
neharepos <nehakeshrimail@gmail.com>
2026-03-01 02:17:38 +0530

    added the logout button

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,24 @@
+"use client";
+import axios from "axios";
+
 export default function ProfilePage(){
+
+    const logout = () => {
+        const logout = () => {
+
+        }
+    }
     return(
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr/>
             <p>Profile page</p>
+
+        <hr/>
+        <button 
+        onClick={logout}
+        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >Logout</button>
         </div>
     )
 }
